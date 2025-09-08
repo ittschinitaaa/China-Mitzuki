2a82b94c3a8620d48eb0e9d42d97d4388c94874f
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-08 11:54:48 -0300

    Update antilink.js

--- commands/antilink.js
+++ commands/antilink.js
@@ -46,7 +46,7 @@ module.exports = async (client, m) => {
       client.sendMessage(
         m.chat,
         {
-          text: `\`[𝐀𝐍𝐓𝐈-𝐋𝐈𝐍𝐊 𝐄𝐒𝐓𝐀 𝐀𝐂𝐓𝐈𝐕𝐎]\`\n\n@${m.sender.split("@")[0]} \nenviaste un enlace...\nSeras eliminado.,
+          text: `\`[𝐀𝐍𝐓𝐈-𝐋𝐈𝐍𝐊 𝐄𝐒𝐓𝐀 𝐀𝐂𝐓𝐈𝐕𝐎]\`\n\n@${m.sender.split("@")[0]} \nenviaste un enlace...\nSeras eliminado.`,
           contextInfo: { mentionedJid: [m.sender] },
         },
         { quoted: m },
